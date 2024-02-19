204747669a0585a8dc3f28e1e174501fb90fbd84
MuhammadAhsan-DataScientist <i201787@nu.edu.pk>
2024-02-19 19:39:41 +0500

    third commit

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import pytest
 from main import StudMlops
 	def test_enroll():
-		enr=StudMlops()
+		enr = StudMlops()
 		enr.enroll(5)
-		total=enr.totals()
-		assert total==5
+		assert enr.totals() == 5
 	def test_drop():
-		dro=StudMlops()
+		dro = StudMlops()
 		dro.enroll(10)
 		dro.drop(3)
-		assert dro.totals()==12
+		assert dro.totals() == 12
 	def test_getclass():
 		names=StudMlops()
-		assert names.getclass()=="mlops"
+		assert names.getclass() == "mlops"
